f1b679ffd172b963811c8295e7d636403ee783fa
Iasmin dos Santos Sanches <74681805+lasmin-dos-santos-sanches@users.noreply.github.com>
2026-02-23 01:57:26 +0000

    adiciona scripts lint:check e lint:fix

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,18 @@
 function Home() {
-    return <h1>Você é muito curioso! Sai Daqui! (Brincadeira, é meu projeto de programação, obrigada por entrar)</h1>
+  return (
+    <h1>
+      Você é muito curioso! Sai Daqui! (Brincadeira, é meu projeto de
+      programação, obrigada por entrar)
+    </h1>
+  );
+}
+
+function teste() {
+  console.log("teste");
+}
+
+function teste2() {
+  console.log("identação errada");
 }
 
 export default Home;
